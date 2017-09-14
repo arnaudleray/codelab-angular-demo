aecc3fd0681245beb9e6c703bd8d6f01683931b8
Arnaud LERAY <arnaud.leray@niji.fr>
2017-09-14 12:36:03 +0200

    step5: finalize EmployeeList

--- src/app/pages/employee-list/employee-list.component.ts
+++ src/app/pages/employee-list/employee-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { EmployeeService } from '../../services/employee.service';
 
 @Component({
   selector: 'app-employee-list',
@@ -7,9 +10,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeeListComponent implements OnInit {
 
-  constructor() { }
+  employees;
+
+  constructor(
+    private router: Router,
+    private employeeService: EmployeeService
+  ) { }
 
   ngOnInit() {
+    this.employees = this.employeeService.getEmployeesMock();
   }
 
+  goToInfo(employee) {
+    this.router.navigate(['/employees', employee.id]);
+  }
 }
