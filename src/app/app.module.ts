983e034ff2cdc48bd7e9732e577d776fc3c16c46
Arnaud LERAY <arnaud.leray@niji.fr>
2017-09-14 12:02:07 +0200

    step2: create EmployeeList and EmployeeDetail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeeInfoComponent } from './pages/employee-info/employee-info.component';
+import { EmployeeListComponent } from './pages/employee-list/employee-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeListComponent,
+    EmployeeInfoComponent
   ],
   imports: [
     BrowserModule,
