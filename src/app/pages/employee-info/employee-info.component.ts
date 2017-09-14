84c28adf15db7de65c1bfa8583c77bc11977d635
Arnaud LERAY <arnaud.leray@niji.fr>
2017-09-14 17:01:43 +0200

    step8: finalize EmployeeInfo (URL parameter + async/await)

--- src/app/pages/employee-info/employee-info.component.ts
+++ src/app/pages/employee-info/employee-info.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { Employee } from '../../models/employee.model';
+import { EmployeeService } from '../../services/employee.service';
+import { UtilsService } from '../../services/utils.service';
 
 @Component({
   selector: 'app-employee-info',
@@ -7,9 +12,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeeInfoComponent implements OnInit {
 
-  constructor() { }
+  employee: Employee;
 
-  ngOnInit() {
+  constructor(
+    private route: ActivatedRoute,
+    private employeeService: EmployeeService,
+    private utils: UtilsService
+  ) { }
+
+  async ngOnInit() {
+    //   this.route.params.subscribe(params => {
+    //     this.employeeService.getEmployee(params['id']).subscribe(
+    //       (employee: Employee) => {
+    //         this.employee = employee;
+    //       },
+    //       (error: any) => {
+    //         alert('ERROR: ' + JSON.stringify(error));
+    //       }
+    //     );
+    //   });
+    try {
+      const params = await this.utils.toPromise(this.route.params);
+      this.employee = await this.employeeService.getEmployee(params['id']);
+    } catch (error) {
+      console.error(error);
+    }
   }
 
 }
