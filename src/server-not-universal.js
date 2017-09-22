8958be4fb5f872bd5edd2b329b52b9aa2c97674f
Arnaud LERAY <arnaud.leray@niji.fr>
2017-09-22 12:45:36 +0200

    fix server-not-universal.js

--- src/server-not-universal.js
+++ src/server-not-universal.js
@@ -1,12 +1,20 @@
+// const compression = require('compression');
+const path = require('path');
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 8080;
 
-// Run the app by serving the static files in the dist directory
-app.use(express.static(__dirname + '/dist'));
+// // Gzip
+// app.use(compression());
+
+// Serve static files from the dist directory
+app.use(express.static(__dirname + '/../dist'));
 
 // Start the app by listening on the default Heroku port
-const HOST = '0.0.0.0';
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, HOST, () => {
-  console.log(`listening on http://${HOST}:${PORT}!`);
+app.listen(port);
+
+// Return index.html for all GET requests for PathLocationStrategy
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/../dist/index.html'));
 });
+console.log(`Server listening on ${port}`);
